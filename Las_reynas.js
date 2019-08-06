b5571c7d7679f56da0bc0bd4f30ea7a8f0f96737
Sergio <telesforociriaco@gmail.com>
2019-08-05 20:26:28 -0500

    ya funciona poner ceros en cualquier posicion v1.0

--- Las_reynas.js
+++ Las_reynas.js
@@ -49,38 +49,13 @@ function llenarCeros(arry) {
                             arry[1][index][y] = 0;
                         }
                     }
-                    // veretical hacia adelante - abajo
-                    for (x,y; y < arry[1][i].length; x++,y++) {
+                    // veretical derecha - abajo
+                    for (x,y; y < arry[1][i].length && x < arry[1][i].length; x++,y++) {
                         if(arry[1][x][y] == 1){
                             continue;
                         }else{
-                            arry[1][x][y] = 0;
-                        }
-                        
-                    }
-                    //console.log(mesh);
-                    x = arry[0][0];
-                    y = arry[0][1];
-
-                    //atras diagonal atras - arriba
-                    for (x,y; x >= 0; x--,y--) {
-                        if(arry[1][x][y] == 1){
-                            continue;
-                        }else{
-                            arry[1][x][y] = 0;
-                        }
-                        
-                    }
-                    //console.log(mesh);
-                    x = arry[0][0];
-                    y = arry[0][1];
-
-                    // vertical hacia atras y abajo
-                    for (x,y; x >= 0; x--,y++) {
-                        if(arry[1][x][y] == 1){
-                            continue;
-                        }else{
-                            arry[1][x][y] = 0;
+                            arry[1][x][y] = 5;
+                            // console.log(mesh);
                         }
                         
                     }
@@ -88,13 +63,42 @@ function llenarCeros(arry) {
                     x = arry[0][0];
                     y = arry[0][1];
 
-
-                    // vertical hacia atras y arriba
-                    for (x,y; y >= 0; x++,y--) {
+                    //diagonal izquierda - arriba
+                    for (x,y; x >= 0 && y >=0; x--,y--) {
                         if(arry[1][x][y] == 1){
                             continue;
                         }else{
-                            arry[1][x][y] = 0;
+                            arry[1][x][y] = 2;
+                            //console.log(mesh);
+                        }
+                        
+                    }
+                   // console.log(mesh);
+                    x = arry[0][0];
+                    y = arry[0][1];
+
+                    // vertical derecha y arriba
+                    for (x,y; y < arry[1][i].length && x >= 0; x--,y++) {
+                        if(arry[1][x][y] == 1){
+                            continue;
+                        }else{
+                            arry[1][x][y] = 3;
+                            //console.log(mesh);
+                        }
+                        
+                    }
+                    //console.log(mesh);
+                    x = arry[0][0];
+                    y = arry[0][1];
+
+
+                    // vertical hacia atras y abajo
+                    for (x,y; x < arry[1][i].length && y >= 0; x++,y--) {
+                        if(arry[1][x][y] == 1){
+                            continue;
+                        }else{
+                            arry[1][x][y] = 4;
+                            //console.log(mesh);
                         }
                         
                     }
@@ -123,5 +127,92 @@ function ponerReina(position, mesh){
 
 }
    
-   
+console.log(queenProblem(8,[0,0]));
+console.log(queenProblem(8,[0,1]));
+console.log(queenProblem(8,[0,2]));
+console.log(queenProblem(8,[0,3]));
+console.log(queenProblem(8,[0,4]));
+console.log(queenProblem(8,[0,5]));
+console.log(queenProblem(8,[0,6]));
+console.log(queenProblem(8,[0,7]));
+  console.log(queenProblem(8,[1,1]));
+  console.log(queenProblem(8,[1,2]));
+  console.log(queenProblem(8,[1,3]));
+  console.log(queenProblem(8,[1,4]));
+  console.log(queenProblem(8,[1,5]));
+  console.log(queenProblem(8,[1,6]));
+  console.log(queenProblem(8,[1,7]));
+  console.log(queenProblem(8,[2,1]));
+  console.log(queenProblem(8,[2,2]));
   console.log(queenProblem(8,[2,3]));
+  console.log(queenProblem(8,[2,4]));
+  console.log(queenProblem(8,[2,5]));
+  console.log(queenProblem(8,[2,6]));
+  console.log(queenProblem(8,[2,7]));
+  console.log(queenProblem(8,[3,1]));
+  console.log(queenProblem(8,[3,2]));
+  console.log(queenProblem(8,[3,3]));
+  console.log(queenProblem(8,[3,4]));
+  console.log(queenProblem(8,[3,5]));
+  console.log(queenProblem(8,[3,6]));
+  console.log(queenProblem(8,[3,7]));
+  console.log(queenProblem(8,[4,1]));
+  console.log(queenProblem(8,[4,2]));
+  console.log(queenProblem(8,[4,3]));
+  console.log(queenProblem(8,[4,4]));
+  console.log(queenProblem(8,[4,5]));
+  console.log(queenProblem(8,[4,6]));
+  console.log(queenProblem(8,[4,7]));
+  console.log(queenProblem(8,[5,1]));
+  console.log(queenProblem(8,[5,2]));
+  console.log(queenProblem(8,[5,3]));
+  console.log(queenProblem(8,[5,4]));
+  console.log(queenProblem(8,[5,5]));
+  console.log(queenProblem(8,[5,6]));
+  console.log(queenProblem(8,[5,7]));
+  console.log(queenProblem(8,[6,1]));
+  console.log(queenProblem(8,[6,2]));
+  console.log(queenProblem(8,[6,3]));
+  console.log(queenProblem(8,[6,4]));
+  console.log(queenProblem(8,[6,5]));
+  console.log(queenProblem(8,[6,6]));
+  console.log(queenProblem(8,[6,7]));
+  console.log(queenProblem(8,[7,1]));
+  console.log(queenProblem(8,[7,2]));
+  console.log(queenProblem(8,[7,3]));
+  console.log(queenProblem(8,[7,4]));
+  console.log(queenProblem(8,[7,5]));
+  console.log(queenProblem(8,[7,6]));
+  console.log(queenProblem(8,[7,7]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  

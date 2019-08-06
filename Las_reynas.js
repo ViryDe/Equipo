c88778399a404e6beef9136c6f8c13d9de738af8
Sergio <telesforociriaco@gmail.com>
2019-08-05 20:43:00 -0500

    avanzando en mi branch

--- Las_reynas.js
+++ Las_reynas.js
@@ -102,7 +102,7 @@ function llenarCeros(arry) {
                         }
                         
                     }
-                    console.log(mesh);
+                    //console.log(mesh);
                     
                 }
             }
@@ -127,63 +127,8 @@ function ponerReina(position, mesh){
 
 }
    
-console.log(queenProblem(8,[0,0]));
-console.log(queenProblem(8,[0,1]));
-console.log(queenProblem(8,[0,2]));
-console.log(queenProblem(8,[0,3]));
-console.log(queenProblem(8,[0,4]));
-console.log(queenProblem(8,[0,5]));
-console.log(queenProblem(8,[0,6]));
-console.log(queenProblem(8,[0,7]));
-  console.log(queenProblem(8,[1,1]));
-  console.log(queenProblem(8,[1,2]));
-  console.log(queenProblem(8,[1,3]));
-  console.log(queenProblem(8,[1,4]));
-  console.log(queenProblem(8,[1,5]));
-  console.log(queenProblem(8,[1,6]));
-  console.log(queenProblem(8,[1,7]));
-  console.log(queenProblem(8,[2,1]));
-  console.log(queenProblem(8,[2,2]));
-  console.log(queenProblem(8,[2,3]));
-  console.log(queenProblem(8,[2,4]));
-  console.log(queenProblem(8,[2,5]));
-  console.log(queenProblem(8,[2,6]));
-  console.log(queenProblem(8,[2,7]));
-  console.log(queenProblem(8,[3,1]));
-  console.log(queenProblem(8,[3,2]));
-  console.log(queenProblem(8,[3,3]));
-  console.log(queenProblem(8,[3,4]));
-  console.log(queenProblem(8,[3,5]));
-  console.log(queenProblem(8,[3,6]));
-  console.log(queenProblem(8,[3,7]));
-  console.log(queenProblem(8,[4,1]));
-  console.log(queenProblem(8,[4,2]));
-  console.log(queenProblem(8,[4,3]));
-  console.log(queenProblem(8,[4,4]));
-  console.log(queenProblem(8,[4,5]));
-  console.log(queenProblem(8,[4,6]));
-  console.log(queenProblem(8,[4,7]));
-  console.log(queenProblem(8,[5,1]));
-  console.log(queenProblem(8,[5,2]));
-  console.log(queenProblem(8,[5,3]));
-  console.log(queenProblem(8,[5,4]));
-  console.log(queenProblem(8,[5,5]));
-  console.log(queenProblem(8,[5,6]));
-  console.log(queenProblem(8,[5,7]));
-  console.log(queenProblem(8,[6,1]));
-  console.log(queenProblem(8,[6,2]));
-  console.log(queenProblem(8,[6,3]));
-  console.log(queenProblem(8,[6,4]));
-  console.log(queenProblem(8,[6,5]));
-  console.log(queenProblem(8,[6,6]));
-  console.log(queenProblem(8,[6,7]));
-  console.log(queenProblem(8,[7,1]));
-  console.log(queenProblem(8,[7,2]));
-  console.log(queenProblem(8,[7,3]));
-  console.log(queenProblem(8,[7,4]));
-  console.log(queenProblem(8,[7,5]));
-  console.log(queenProblem(8,[7,6]));
-  console.log(queenProblem(8,[7,7]));
+
+ 
 
 
 
